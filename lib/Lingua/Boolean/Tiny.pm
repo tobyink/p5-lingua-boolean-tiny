fe431ed53f0c4a827262e813adf8e9daa121c794
Toby Inkster <mail@tobyinkster.co.uk>
2013-03-05 23:12:32 +0000

    initial checkin

---
+++ lib/Lingua/Boolean/Tiny.pm
@@ -0,0 +1,417 @@
+use 5.008;
+use strict;
+use warnings;
+use utf8;
+
+{
+	package Lingua::Boolean::Tiny;
+	
+	use Exporter 'import';
+	
+	our $AUTHORITY = 'cpan:TOBYINK';
+	our $VERSION   = '0.001';
+	our @EXPORT    = qw( boolean );
+	our (%LANG, @LANG);
+	
+	sub new
+	{
+		require Carp;
+		my $class = shift;
+		my $self  = $class->new_strict(@_)
+			or Carp::carp("Language not available, using English instead");
+		$self || "Lingua::Boolean::Tiny"->new_strict("en");
+	}
+	
+	sub new_strict
+	{
+		shift;
+		my ($lang) = @_;
+		return "Lingua::Boolean::Tiny::Union"->new(@$lang) if ref($lang);
+		return "Lingua::Boolean::Tiny::Union"->new(@LANG) if !defined $lang;
+		my $class = $LANG{ lc $lang };
+		return $class->new if $class;
+		return;
+	}
+	
+	sub boolean
+	{
+		my $text = shift;
+		my $lang = __PACKAGE__->new(@_ ? $_[0] : \@LANG);
+		$lang->boolean($text);
+	}
+	
+	sub langs
+	{
+		@LANG;
+	}
+	
+	sub languages
+	{
+		map __PACKAGE__->new($_)->languages, @LANG;
+	}
+}
+
+{
+	package Lingua::Boolean::Tiny::BASE;
+	
+	our $AUTHORITY = 'cpan:TOBYINK';
+	our $VERSION   = '0.001';
+	
+	# Versions of ~~ and fc for legacy Perls...
+	use if $] >= 5.016, feature => 'fc';
+	BEGIN { eval q[fc("1")] or eval q[sub fc($) { lc $_[0] }] };
+	
+	eval q
+	{
+		use 5.010;
+		sub match
+		{
+			my ($a, $b) = @_;
+			$a ~~ $b;
+		}
+		1;
+	}
+	or eval q
+	{
+		sub match
+		{
+			my ($a, $b) = @_;
+			if (ref($b) eq 'ARRAY')
+			{
+				for my $b2 (@$b)
+				{
+					return 1 if match($a, $b2);
+				}
+			}
+			ref($b) eq 'Regexp' and return $a =~ $b;
+			ref($b) eq 'CODE'   and return $b->($a);
+			return $a eq $b;
+		}
+	};
+	
+	sub boolean
+	{
+		my $self = shift;
+		my ($text) = @_;
+		
+		$text =~ s/(?:^\s+)|(?:\s+$)//gs;
+		
+		return 1 if fc $text eq fc $self->yes;
+		return 0 if fc $text eq fc $self->no;
+		return 1 if match($text, $self->yes_expr);
+		return 0 if match($text, $self->no_expr);
+		return 1 if fc $text eq fc 'y';
+		return 0 if fc $text eq fc 'n';
+		return 1 if $text eq 1;
+		return 0 if $text eq 0;
+		return undef;
+	}
+	
+	sub make_classes
+	{
+		my $base = shift;
+		no strict 'refs';
+		for (@_)
+		{
+			my ($lang, $codes, $yes, $no, $yes_expr, $no_expr) = @$_;
+			$yes_expr = $yes unless defined $yes_expr;
+			$no_expr  = $no  unless defined $no_expr;
+			my $class = "Lingua::Boolean::Tiny::$lang";
+			${"$class\::AUTHORITY"} = $AUTHORITY;
+			${"$class\::VERSION"}   = $VERSION;
+			@{"$class\::ISA"}       = ($base);
+			eval qq
+			{
+				package $class;
+				sub new       { my \$k = shift; bless qr{$lang}, \$k };
+				sub yes       { \$yes };
+				sub no        { \$no };
+				sub yes_expr  { \$yes_expr };
+				sub no_expr   { \$no_expr };
+				sub languages { \$lang };
+				sub langs     { \@{ \$codes } };
+			};
+			for my $lang (@$codes)
+			{
+				$Lingua::Boolean::Tiny::LANG{$lang} = $class;
+			}
+			push @Lingua::Boolean::Tiny::LANG, $codes->[0];
+		}
+		
+		return 1;
+	}
+}
+
+{
+	package Lingua::Boolean::Tiny::Union;
+	
+	our $AUTHORITY = 'cpan:TOBYINK';
+	our $VERSION   = '0.001';
+	
+	sub new
+	{
+		my $class = shift;
+		bless [
+			map  $_->new,
+			grep defined,
+			map  $Lingua::Boolean::Tiny::LANG{lc $_}, @_
+		] => $class;
+	}
+	
+	sub boolean
+	{
+		my $self = shift;
+		for (@$self)
+		{
+			my $r = $_->boolean(@_);
+			return $r if defined $r;
+		}
+		return undef;
+	}
+	
+	sub langs
+	{
+		my $self = shift;
+		map $_->langs, @$self;
+	}
+	
+	sub languages
+	{
+		my $self = shift;
+		map $_->languages, @$self;
+	}
+}
+
+"Lingua::Boolean::Tiny::BASE"->make_classes(
+	[
+		Chinese => [qw( zh zho chi )],
+		q/是。/ => q/不是。/,
+		[qw( 是。 對。 是。 对。 ), qr{^[ds]h[ìi]$}i ],
+		[qw( 不是。 不對。 不是。 不对。 ), qr{^b[úu]\s*[ds]h[ìi]$}i ],
+	],
+	[
+		English => [qw( en eng )],
+		qw( yes no ),
+		[qr{^y(?:es)?$}i, qr{^on$}i, qr{^ok$}i, qr{^true$}i],
+		[qr{^no?$}i, qr{^off$}i, qr{not ?ok$}i, qr{^false$}i],
+	],
+	[
+		Spanish => [qw( es spa )],
+		qw( sí no ),
+		qr{^s[íi]$}i,
+		qr{^no$}i,
+	],
+	[
+		Hindi => [qw( hi hin )],
+		q/हाँ/ => q/नहीं/,
+		[ "हाँ", qr{^h[aā]̃$}i, "जी", qr{^j[īi]$}i, "जी हाँ", qr{^j[īi]\s*h[aā]̃$}i, qr{^ji\s*ha$}i ],
+		[ "नहीं", qr{^nahī̃$}i, "जी नहीं", qr{^jī nahī̃$}i, qr{^ji\s*nahi$}i ],
+	],
+	[
+		Russian => [qw( ru rus )],
+		qw( да нет ),
+		[ qr{^да$}i, qr{^da$}i ],
+		[ qr{^нет$}i, qr{^n[iy]?et$}i ],
+	],
+	[
+		Arabic => [qw( ar ara )],
+		q/نعم/ => q/ﻻ/,
+	],
+	[
+		Portuguese  => [qw( pt por )],
+		qw( sim não ),
+		[qr{^sim$}i],
+		[qr{^n[aã]o$}i],
+	],
+	[
+		Bengali => [qw( bn ben )],
+		q/জি/ => q/না/,
+		[q/জি/, q/হ্যাঁ/, qr{^ji$}i, qr{^h[eê][nñ]$}i],
+		[q/না/, qr{^n[aā]$}i],
+	],
+	[
+		French => [qw( fr fre fra )],
+		qw( oui non ),
+		[qr{^oui$}i, qr{^ok$}i, qr{^vraie?$}i],
+		[qr{^n(?:on?)?$}i, qr{^faux$}i],
+	],
+	[
+		Malay => [qw( ms may msa )],
+		qw( ya tidak ),
+		[qr{^ya$}i, qr{^ha'?ah$}i],
+		[qr{^tidak$}i, qr{^tak$}i],
+	],
+	[
+		German => [qw( de deu ger )],
+		qw( ja nein ),
+	],
+	[
+		Japanese => [qw( ja jpn )],
+		q/はい/ => q/いいえ/,
+		[q/はい/, q/ええ/, q/うん/, qr{^hai$}i, qr{^[eē]$}i, qr{^un$}i, qr{^n̄$}i],
+		[q/いいえ/, q/いえ/, q/ううん/, q/違う/, qr{^[íi]?ie$}i, qr{^uun$}i, qr{^[n̄n]n$}i, qr{^chigau$}i],
+	],
+);
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Lingua::Boolean::Tiny - a smaller Lingua::Boolean, with support for more languages
+
+=head1 SYNOPSIS
+
+   use 5.010;
+   use Lingua::Boolean::Tiny qw( boolean );
+   
+   my $response = "ja";  # German for "yes"
+   
+   if (boolean $response) {
+      say "Yes!";
+   }
+   else {
+      say "No!";
+   }
+
+=head1 DESCRIPTION
+
+This module provides an API roughly compatible with L<Lingua::Boolean> but
+has no non-core dependencies, supports Perl 5.8 (any earlier and the Unicode
+support is terrible), supports the world's twelve most commonly spoken
+languages (Standard Chinese, English, Castillian Spanish, Hindi, Russian,
+Arabic, Portuguese, Bengali, French, Malay, German and Japanese), and adding
+support for other languages is easy peasy.
+
+The strings "Y" and "1" are always true, and "N" and "0" are always false.
+
+=head2 Object-Oriented Interface
+
+=head3 Constructor
+
+=over
+
+=item C<< Lingua::Boolean::Tiny->new($lang) >>
+
+Construct a new object supporting the given language. C<$lang> should be an
+ISO language code (e.g. "en" for English or "zh" for Chinese).
+
+If the language is not recognised, a warning is issued and an object with
+support for just English is returned.
+
+=item C<< Lingua::Boolean::Tiny->new(\@lang) >>
+
+Construct a new object supporting the union of multiple languages.
+
+Unrecognised languages are simply ignored.
+
+Because a string could be interpreted differently in different languages
+(e.g. "no" is a negative answer in English, but affirmative in Polish), the
+order is significant - in case of ambiguities, the earlier language wins.
+
+=item C<< Lingua::Boolean::Tiny->new() >>
+
+Construct a new object supporting the union of all supported languages.
+
+=item C<< Lingua::Boolean::Tiny->new_strict($lang) >>
+
+Like C<< new >>, but rather than defaulting to English, returns undef.
+
+=back
+
+You can alternatively construct objects using class names based on the
+language name:
+
+   my $indonesian = Lingua::Boolean::Tiny::Malay->new();
+
+=head3 Methods
+
+=over
+
+=item C<< boolean($text) >>
+
+Returns true if the text seems to indicate an affirmative answer (e.g. "yes");
+returns false if the text seems to indicate an negitive answer (e.g. "no");
+returns undef if the meaning of the text could not be established.
+
+=item C<< languages >>
+
+Returns the name of the languages supported by this object.
+
+=item C<< langs >>
+
+Returns the ISO codes of the languages supported by this object.
+
+=back
+
+=head2 Functional Interface
+
+This module provides several functions:
+
+=over
+
+=item C<< boolean $text, $lang >>
+
+Shortcut for:
+
+   Lingua::Boolean::Tiny->new($lang)->boolean($text)
+
+C<< $lang >> is optional, but may be an ISO language code or a union thereof.
+
+This function is exported by default.
+
+=item C<< Lingua::Boolean::Tiny::languages() >>
+
+Returns the full names of all supported languages.
+
+This function is not exported.
+
+=item C<< Lingua::Boolean::Tiny::langs() >>
+
+Returns the ISO codes of all supported languages.
+
+This function is not exported.
+
+=back
+
+=head2 Adding languages
+
+Here's how you'd add support for Italian:
+
+   "Lingua::Boolean::Tiny::BASE"->make_classes([
+      Italian => [qw( it ita )],     # Language name and ISO codes
+      "sì",                          # Canonical "yes"
+      "no",                          # Canonical "no"
+      [ qr{^sì$}i ],                 # Other things that match "yes"
+      [ qr{^no$}i ],                 # Other things that match "no"
+   ]);
+
+Easy peasy!
+
+=head1 BUGS
+
+Please report any bugs to
+L<http://rt.cpan.org/Dist/Display.html?Queue=Lingua-Boolean-Tiny>.
+
+=head1 SEE ALSO
+
+L<Lingua::Boolean>, L<String::BooleanSimple>, L<I18N::Langinfo>.
+
+=head1 AUTHOR
+
+Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
+
+=head1 COPYRIGHT AND LICENCE
+
+This software is copyright (c) 2013 by Toby Inkster.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=head1 DISCLAIMER OF WARRANTIES
+
+THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
