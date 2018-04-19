1290234732256169d836d2e2c6a028f7477c6e2a
Toby Inkster <mail@tobyinkster.co.uk>
2018-04-19 21:16:39 +0100

    bump version; changelog

--- lib/Lingua/Boolean/Tiny/More.pm
+++ lib/Lingua/Boolean/Tiny/More.pm
@@ -1628,7 +1628,7 @@ my $base = do {
 	package Lingua::Boolean::Tiny::More;
 	our @ISA = qw(Lingua::Boolean::Tiny::BASE);
 	our $AUTHORITY = "cpan:TOBYINK";
-	our $VERSION   = "0.007";
+	our $VERSION   = "0.008";
 	sub _r {
 		my $base   = shift;
 		my $caller = shift || caller;
